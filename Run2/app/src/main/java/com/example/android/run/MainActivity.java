8730fc6f2ce39182390b399ef1c232b31a8d1b29
陳玉璇 <amy10260903@yahoo.com.tw>
2017-03-26 15:57:07 +0800

    map mission more layout

---
+++ Run2/app/src/main/java/com/example/android/run/MainActivity.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.run;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.graphics.drawable.VectorDrawableCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.R.attr.label;
+import static android.R.attr.tag;
+
+
+/**
+ * Provides UI for the main screen.
+ */
+public class MainActivity extends FragmentActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //獲取TabHost控制元件
+        FragmentTabHost mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        //設定Tab頁面的顯示區域，帶入Context、FragmentManager、Container ID
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.container);
+
+        /**
+         新增Tab結構說明 :
+         首先帶入Tab分頁標籤的Tag資訊並可設定Tab標籤上顯示的文字與圖片，
+         再來帶入Tab頁面要顯示連結的Fragment Class，最後可帶入Bundle資訊。
+         **/
+
+        //小黑人建立一個Tab，這個Tab的Tag設定為one，
+        //並設定Tab上顯示的文字為第一堂課與icon圖片，Tab連結切換至
+        //LessonOneFragment class，無夾帶Bundle資訊。
+        mTabHost.addTab(mTabHost.newTabSpec("one")
+                        .setIndicator("",getResources().getDrawable(R.drawable.ic_maps_2))
+                ,MapsFragment.class,null);
+
+        //同上方Tab設定，不同處為帶入參數的差異
+        mTabHost.addTab(mTabHost.newTabSpec("two")
+                        .setIndicator("",getResources().getDrawable(R.drawable.ic_missions))
+                ,MissionsFragment.class,null);
+
+        //同上方Tab設定，不同處為帶入參數的差異
+        mTabHost.addTab(mTabHost.newTabSpec("three")
+                        .setIndicator("",getResources().getDrawable(R.drawable.ic_bag))
+                ,BagFragment.class, null);
+
+        //同上方Tab設定，不同處為帶入參數的差異
+        mTabHost.addTab(mTabHost.newTabSpec("four")
+                        .setIndicator("",getResources().getDrawable(R.drawable.ic_more))
+                ,MoreFragment.class,null);
+    }
+
+    /**
+     方法權限設定為Public目的是可以讓Fragment取得內容 。
+     */
+
+    //Tab - Maps的文字內容
+    public String getMapsText()
+    {
+        return "Maps";
+    }
+
+    //Tab - Missions的文字內容
+    public String getMissionsText()
+    {
+        return "Missions Name";
+    }
+
+    //Tab - Bag的文字內容
+    public String getBagText()
+    {
+        return "Bag";
+    }
+
+    //Tab - More的文字內容
+    public String getMoreText()
+    {
+        return "More";
+    }
+
+}
---
+++ Run2/app/src/main/java/com/example/android/run/MissionsFragment.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.run;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Provides UI for the view with Tile.
+ */
+public class MissionsFragment extends Fragment
+{
+    //顯示文字內容
+    private String text = "";
+
+    @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+
+        //取得MainActivity的方法，將文字放入text字串
+        MainActivity mMainActivity = (MainActivity) activity;
+        text = mMainActivity.getMissionsText();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        //導入Tab分頁的Fragment Layout
+        return inflater.inflate(R.layout.item_missions, container, false);
+    }
+    /*@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
+                R.layout.recycler_view, container, false);
+        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return recyclerView;
+    }*/
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+
+        //取得TextView元件並帶入text字串
+        TextView mText = (TextView) getView().findViewById(R.id.list_title);
+        mText.setText(text);
+
+        //取得TextView元件並帶入text字串
+        TextView mText2 = (TextView) getView().findViewById(R.id.list_time);
+        mText2.setText("03:00");
+
+        //取得ImageView元件並帶入指定圖片
+        ImageView mImg = (ImageView) getActivity().findViewById(R.id.list_avatar);
+        mImg.setImageResource(R.drawable.missions_main);
+
+        //取得ImageView元件並帶入指定圖片
+        ImageView mImg2 = (ImageView) getActivity().findViewById(R.id.list_check);
+        mImg2.setImageResource(R.drawable.missions_check);
+
+    }
+
+    /*public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView avator;
+        public TextView name;
+        public TextView description;
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.item_missions, parent, false));
+            avator = (ImageView) itemView.findViewById(R.id.list_avatar);
+            name = (TextView) itemView.findViewById(R.id.list_title);
+            description = (TextView) itemView.findViewById(R.id.list_time);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = v.getContext();
+                    //Intent intent = new Intent(context, DetailActivity.class);
+                    //intent.putExtra(DetailActivity.EXTRA_POSITION, getAdapterPosition());
+                    //context.startActivity(intent);
+                }
+            });
+        }
+    }*/
+
+    /**
+     * Adapter to display recycler view.
+     */
+    /*public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+        // Set numbers of List in RecyclerView.
+        private static final int LENGTH = 18;
+
+        private final String[] mPlaces;
+        private final String[] mPlaceDesc;
+        private final Drawable[] mPlaceAvators;
+
+        public ContentAdapter(Context context) {
+            Resources resources = context.getResources();
+            mPlaces = resources.getStringArray(R.array.places);
+            mPlaceDesc = resources.getStringArray(R.array.place_desc);
+            TypedArray a = resources.obtainTypedArray(R.array.place_avator);
+            mPlaceAvators = new Drawable[a.length()];
+            for (int i = 0; i < mPlaceAvators.length; i++) {
+                mPlaceAvators[i] = a.getDrawable(i);
+            }
+            a.recycle();
+        }
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.avator.setImageDrawable(mPlaceAvators[position % mPlaceAvators.length]);
+            holder.name.setText(mPlaces[position % mPlaces.length]);
+            holder.description.setText(mPlaceDesc[position % mPlaceDesc.length]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return LENGTH;
+        }
+    }*/
+}
