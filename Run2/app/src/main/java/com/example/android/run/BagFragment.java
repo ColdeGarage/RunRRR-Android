df697235a0267eec24b0ac7de5fcfc90f3e62dd4
陳玉璇 <amy10260903@yahoo.com.tw>
2017-08-02 15:23:36 +0800

    BagPopActivity replace window.

--- Run2/app/src/main/java/com/example/android/run/BagFragment.java
+++ Run2/app/src/main/java/com/example/android/run/BagFragment.java
@@ -328,7 +328,7 @@ public class BagFragment extends Fragment
                     //TODO:Intent to other activity
                     Context context = vv.getContext();
 
-                    Intent intent = new Intent(context, BagPopWindowActivity.class);
+                    Intent intent = new Intent(context, BagPopActivity.class);
 
                     Bundle bundle=new Bundle();
 
@@ -353,7 +353,7 @@ public class BagFragment extends Fragment
                         //TODO:Intent to other activity
                         Context context = vv.getContext();
 
-                        Intent intent = new Intent(context, BagPopWindowActivity.class);
+                        Intent intent = new Intent(context, BagPopActivity.class);
 
                         Bundle bundle=new Bundle();
                         bundle.putString("uid",String.valueOf(uid));
