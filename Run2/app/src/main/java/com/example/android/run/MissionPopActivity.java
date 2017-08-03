501eca7fbb770b702e97e32c8e5faca191ab8ba4
陳玉璇 <amy10260903@yahoo.com.tw>
2017-08-03 13:07:24 +0800

    MapsFragment layout point bug fixed,

--- Run2/app/src/main/java/com/example/android/run/MissionPopActivity.java
+++ Run2/app/src/main/java/com/example/android/run/MissionPopActivity.java
@@ -10,7 +10,6 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -43,8 +42,6 @@ public class MissionPopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_missions_pop);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 //        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
