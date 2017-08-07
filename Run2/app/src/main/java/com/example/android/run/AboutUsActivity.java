c9039feb409cd9fa2fe0ff734a6a15d4ec7a5836
陳玉璇 <amy10260903@yahoo.com.tw>
2017-08-07 11:32:10 +0800

    AboutUs layout from login modified.

--- Run2/app/src/main/java/com/example/android/run/AboutUsActivity.java
+++ Run2/app/src/main/java/com/example/android/run/AboutUsActivity.java
@@ -1,18 +1,29 @@
 package com.example.android.run;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
-public class AboutUsActivity extends AppCompatActivity {
+public class AboutUsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us_from_login);
         TextView title = (TextView) findViewById(R.id.about_us_title);
+        TextView content = (TextView) findViewById(R.id.about_us_content);
+        TextView cancelButton = (TextView) findViewById(R.id.btn_cancel);
         title.setText("ABOUT US");
+        content.setText(R.string.aboutUs);
+
+        cancelButton.setOnClickListener(new TextView.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
